86847c0d2082812352b984ed7594ea4ae49111b0
molybdenum1 <kozelden4ik@gmail.com>
2022-12-10 19:26:57 +0200

    feat: adding task

---
+++ src/modals/CreateTask.tsx
@@ -0,0 +1,49 @@
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {useState} from 'react';
+
+const CreateTask = ({modal, toggle} : {modal: boolean, toggle: React.MouseEventHandler<HTMLButtonElement> | undefined}) => {
+    const [name, setName] = useState('');
+    const [desc, setDesc] = useState('');
+    
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        const name= e.target.name;
+        const value = e.target.value;
+        if(name === 'taskName'){ 
+            setName(value)
+        }else{ 
+            setDesc(value)
+        }
+    }
+    
+    return ( 
+        <Modal isOpen={modal} toggle={toggle}>
+          <ModalHeader toggle={toggle}>Create Task</ModalHeader>
+          <ModalBody>
+            <form>
+                <div className='form-group'>
+                    <label>Task Name</label>
+                    <input type="text"
+                    name='taskName'
+                    onChange={handleChange}
+                    className='form-control' 
+                    value={name} />
+                </div>
+                <div className='form-group'>
+                    <label>Task Description</label>
+                    <textarea 
+                    name='taskDesc'
+                    onChange={handleChange}
+                    value={desc} 
+                    className='form-control'></textarea>
+                </div>
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={toggle}>Create</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+     );
+}
+ 
+export default CreateTask;
